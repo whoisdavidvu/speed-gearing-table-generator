4fd3e9a4187fcee3a91b5256d7e81dd43f7ac12e
David Vu <davidvu396@googlemail.com>
2021-11-29 13:09:42 +0100

    fixed class name in file

--- createTable.java
+++ createTable.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.Math;
 
-public class Functions
+public class createTable
 {
     public static Scanner sc = new Scanner (System.in);
 
